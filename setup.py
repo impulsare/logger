0295c92f46cd20ae17b32e488290b31738a739fb
Emmanuel Dyan <emmanuel.dyan@inetprocess.com>
2017-08-10 22:21:28 +0200

    Version b3 with default directory for logs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = '1.0b2'
+__version__ = '1.0b3'
 
 here = path.abspath(path.dirname(__file__))
 
