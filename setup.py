35bf8ecbf4eaa58aca25b2e9b159728f79cbff18
Emmanuel Dyan <emmanuel.dyan@inetprocess.com>
2017-08-10 16:47:20 +0200

    don't download config from github but from pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = '1.0b1'
+__version__ = '1.0b2'
 
 here = path.abspath(path.dirname(__file__))
 
