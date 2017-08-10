315214ad23df927beec9ce6216d0b0baa301f078
Emmanuel Dyan <emmanuel.dyan@inetprocess.com>
2017-08-10 16:17:15 +0200

    first commit

---
+++ tests/test_logger.py
@@ -0,0 +1,53 @@
+import logging
+import os
+import sys
+import unittest
+
+from impulsare_logger import Logger
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, base_dir + '/../')
+
+
+# https://docs.python.org/3/library/unittest.html#assert-methods
+class TestLogger(unittest.TestCase):
+    def test_config_not_exists(self):
+        with self.assertRaisesRegex(IOError, 'Missing config file: ".+" does not exist'):
+            Logger('pytest', '/does/not/exist')
+
+
+    def test_log_not_writeable(self):
+        with self.assertRaisesRegex(IOError, "Can't write to '.+writeable.+pytest\.log'"):
+            Logger('pytest', base_dir + '/static/config_notwriteable.yml')
+
+
+    def test_log_config_invalid(self):
+        with self.assertRaisesRegex(ValueError, "Your config is not valid: True is not of type 'string'"):
+            Logger('pytest', base_dir + '/static/config_invalid.yml')
+
+
+    def test_log_config_valid(self):
+        if os.path.isfile('/tmp/test.log'):
+            os.remove('/tmp/test.log')
+
+        logger = Logger('pytest', base_dir + '/static/config_valid.yml')
+
+        logger.log.warning('Hello')
+        fileobj = open(logger.filename, 'r')
+        self.assertRegex(fileobj.readline(), '.+ :: WARNING :: Hello')
+
+        # Not working, see later
+        # out, err = capsys.readouterr()
+        # self.assertEqual('Hello', out)
+
+        os.remove(logger.filename)
+
+        logger.log.debug('Not here')
+        self.assertFalse(os.path.isfile(logger.filename))
+
+        # clean handlers
+        logging.getLogger('pytest').handlers = []
+
+
+if __name__ == "__main__":
+    unittest.main()
